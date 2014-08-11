c4fc4db15aba74698737b8b43217dcdcdc77d8f0
Randall <randall.krauskopf@gmail.com>
2014-08-11 11:06:01 -0500

    -Moved local authentication from viewRoutes to authRoutes

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,27 +1,70 @@
 module.exports = function(router, passport) {
 
+
+    //// =====================================
+    // LOCAL SIGNUP =====================
+    // =====================================
+
+    router.post('/auth/signup', function(req, res, next) {
+        passport.authenticate('local-user-signup', function(err, user, info) {
+            if (err) { return next(err); }
+            if (!user) {
+                return res.send('406', 'User already exists');
+            }
+            req.logIn(user, function(err) {
+                if (err) { return next(err); }
+                return res.send('201', 'User Created');
+            });
+        })(req, res, next);
+    });
+    //// =====================================
+    // LOCAL LOGIN =====================
+    // =====================================
+
+    router.post('/auth/login', function(req, res, next) {
+        passport.authenticate('local-login', function(err, user, info) {
+            if (err) { return next(err); }
+            if (!user) {
+                return res.send('401', 'Failed login!');
+            }
+            req.logIn(user, function(err) {
+                if (err) { return next(err); }
+                return res.send('200', 'Successful Login!');
+            });
+        })(req, res, next);
+    });
+
     //// =====================================
     // SIGN OUT ROUTES =====================
     // =====================================
 
-    router.get('/auth/logout', function(req, res) {
-       req.logout();
-       res.send('200', 'User logged out');
+    router.get('/auth/logout', function (req, res) {
+        req.logout();
+        res.send('200', 'User logged out');
     });
 
     // =====================================
     // FACEBOOK ROUTES =====================
     // =====================================
     // route for facebook authentication and login
-    router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+    router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
 
     // handle the callback after facebook has authenticated the user
     router.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
+            successRedirect: '/profile',
+            failureRedirect: '/'
         }));
 
+    /*
+     router.get('/auth/facebook/callback',
+     passport.authenticate('facebook', function(err, user, info){
+     if(err) {return next(err);}
+
+     res.send('200', 'Successful login!');
+     }));
+     */
     // =====================================
     // TWITTER ROUTES ======================
     // =====================================
@@ -31,8 +74,8 @@ module.exports = function(router, passport) {
     // handle the callback after twitter has authenticated the user
     router.get('/auth/twitter/callback',
         passport.authenticate('twitter', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
+            successRedirect: '/profile',
+            failureRedirect: '/'
         }));
 
 
@@ -42,13 +85,22 @@ module.exports = function(router, passport) {
     // send to google to do the authentication
     // profile gets us their basic information including their name
     // email gets their emails
-    router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+    router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
     // the callback after google has authenticated the user
-    router.get('/auth/google/callback',
-        passport.authenticate('google', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
-        }));
+    /*
+     router.get('/auth/google/callback',
+     passport.authenticate('google', {
+     successRedirect : '/profile',
+     failureRedirect : '/'
+     }));
+     */
+    router.get('/auth/google/callback', function (req, res, next) {
+        passport.authenticate('google', function (err, user, info) {
+            if(err) {return next(err); }
 
+            return res.send('200', 'Successful Login!');
+
+        })(req, res, next);
+    });
 }
