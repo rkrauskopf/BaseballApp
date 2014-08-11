8d1b8781676777898f570132e0ba956880d56344
Randall <randall.krauskopf@gmail.com>
2014-08-11 15:23:33 -0500

    -Added client support for an admin to view current Trainers and Customers

--- public/js/controllers/CustomerListCtrl.js
+++ public/js/controllers/CustomerListCtrl.js
@@ -1,3 +1,11 @@
 angular.module('CustomerListCtrl', []).controller('CustomerListCtrl', function($scope, $rootScope, $http, $location) {
+    $scope.customers = null;
 
+    $http.get('/api/customers')
+        .success(function(customers) {
+            $scope.customers = customers;
+        })
+        .error(function(errMessage) {
+
+        });
 });
--- public/js/controllers/TrainerListCtrl.js
+++ public/js/controllers/TrainerListCtrl.js
@@ -1,3 +1,11 @@
 angular.module('TrainerListCtrl', []).controller('TrainerListCtrl', function($scope, $rootScope, $http, $location) {
+    $scope.trainers = null;
 
+    $http.get('/api/trainers')
+        .success(function(trainers) {
+            $scope.trainers = trainers;
+        })
+        .error(function(errMessage) {
+
+        });
 });
